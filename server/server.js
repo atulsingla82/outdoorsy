ef7459f5cdb9225a1eb5fed79a0ca4a3853fbd45
rutweb5d <clauderobo@outlook.com>
2017-07-19 00:30:29 -0400

    Commit 07/19/17 12:30 AM

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(express.static("./public"));
 
 // MongoDB Configuration configuration (Change this URL to your own DB)
 //mongoose.connect("mongodb://admin:codingrocks@ds023664.mlab.com:23664/reactlocate");
-mongoose.connect("mongodb://127.0.0.1:27017")
+mongoose.connect("mongodb://127.0.0.1:27017/outdoorsy")
 var db = mongoose.connection;
 
 db.on("error", function(err) {
