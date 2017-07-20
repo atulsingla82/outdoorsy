6fb109de48833e3d8379aeb583ae78a32aba8a8c
Andrew Zahler <andrew.zahler@gmail.com>
2017-07-20 16:00:17 -0400

    moving code from SearchForm.js to App.js

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,49 +1,31 @@
 import React, { Component } from "react";
 import { ButtonToolbar, Button, ButtonGroup, Well } from 'react-bootstrap';
-import loadGoogleMapsAPI from 'load-google-maps-api';
-import { Link, Route, BrowserRouter as Router } from 'react-router-dom';
-
-
+/*import loadGoogleMapsAPI from 'load-google-maps-api';
+*/import { Link, Route, BrowserRouter as Router } from 'react-router-dom';
 
 import '../styles/searchform.css';
 
 class SearchForm extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.getLocation = this.getLocation.bind(this);
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-            this.state = {
-                lat: null,
-                lng: null,
-                activity: "",
-                searchRadius: null,
-          apiLoaded: false,
-          results: []
+        this.state = {
+            lat: null,
+            lng: null,
+            activity: "",
+            searchRadius: null,
+            results: []
         }
   }
 
-    componentDidMount() {
-    /* Loads the Google Maps API with Places library into this component */    
-    loadGoogleMapsAPI({
-      key: 'AIzaSyCUX8t_7WDEjYpCH34o4MPRPREZi_HpOzo',
-      libraries: ["places"]
-    })
-    .then((googleAPI) => {
-          this.setState({apiLoaded: true, googleAPI: googleAPI});
-        }).catch((err) => {
-          console.error(err)
-    })
-  }
-
     componentDidUpdate() {
-        this.getLocation()
+        this.getLocation();
     }
 
-    // method to handle autocomplete in search bar and capturing lat/lng data from selected location
     getLocation() {
-        const googleAPI = this.state.googleAPI;
-        console.log("The Google API", googleAPI)
+        const googleAPI = this.props.googleAPI;
         const input = document.getElementById('autocomplete');
         const defaultBounds = new googleAPI.LatLngBounds(
             googleAPI.LatLng(-90, -180),
@@ -54,16 +36,12 @@ class SearchForm extends Component {
         }
         const autocomplete = new googleAPI.places.Autocomplete(input, options);
         const onPlaceChanged = () => {
-            /*get the place*/
-        const place = autocomplete.getPlace();
-        console.log(place);
-        /*get lng/lat for place*/
-        const placeLat = place.geometry.location.lat();
-        const placeLng = place.geometry.location.lng();
-        console.log(placeLat, placeLng); 
-        this.setState({lat: placeLat, lng: placeLng})
-      }
-      autocomplete.addListener('place_changed', onPlaceChanged);
+            const place = autocomplete.getPlace();
+            const placeLat = place.geometry.location.lat();
+            const placeLng = place.geometry.location.lng();
+            this.setState({lat: placeLat, lng: placeLng});
+        }
+         autocomplete.addListener('place_changed', onPlaceChanged);
     }
 
     /*  Grabs values from input button groups and stores them in state */
@@ -71,34 +49,18 @@ class SearchForm extends Component {
         let newState = {};
         newState[event.target.name] = event.target.value;
         this.setState(newState);
+        console.log(this.state);
     }
 
     /* Gets triggered when a user clicks on the submit button in the search form
     /* Stores queryresults in state */
     handleSubmit(event) {
         event.preventDefault();
-        const queryPlaces = () => { 
-        const googleAPI = this.state.googleAPI;
-        let service;
-        const initialize = () => {
-          const locationCoords = {lat: this.state.lat, lng: this.state.lng};
-          const request = {
-            location: locationCoords,
-            radius: this.state.searchRadius,
-            keyword: [this.state.activity]
-          };    
-          service = new googleAPI.places.PlacesService(document.createElement('div.attributions'));
-          let callback = (results, status) => {
-            if (status === googleAPI.places.PlacesServiceStatus.OK) {
-              console.log(results);
-              this.setState({results: results});
-            }
-          }
-          service.nearbySearch(request, callback);      
-        }
-        initialize();
-      }
-      queryPlaces();
+        const newLat = this.state.lat;
+        const newLng = this.state.lng;
+        const newActivity = this.state.activity;
+        const newSearchRadius = this.state.searchRadius;
+        this.props.setParentLocation(newLat, newLng, newActivity, newSearchRadius);
     }
 
     render() {
@@ -117,7 +79,7 @@ class SearchForm extends Component {
             <div className = "container-button">
                 <ButtonGroup bsSize = "large">
                     <Button 
-                         type="checkbox"
+                        type="checkbox"
                         name="activity"
                         value = "hiking"
                         onClick = { this.handleInputChange }>
@@ -133,7 +95,7 @@ class SearchForm extends Component {
                     <Button 
                          type="checkbox"
                         name="activity"
-                        value=""
+                        value="camping"
                         onClick = { this.handleInputChange }>
                         Camping 
                     </Button> 
@@ -153,29 +115,34 @@ class SearchForm extends Component {
                     </Button> 
                   </ButtonGroup>
             </div>
+            <div>
             <ButtonGroup bsSize = "large">
                 <Button
+                    type="checkbox"
                     name="searchRadius"
                     value={16094}
                     onClick = { this.handleInputChange }>
                     10 miles 
                  </Button> 
                 <Button
+                    type="checkbox"
                     name="searchRadius"
                     value={32187}
                     onClick = { this.handleInputChange }> 
                     20 miles 
                 </Button> 
                 <Button
+                    type="checkbox"
                     name="searchRadius"
                     value={48280}
                     onClick = { this.handleInputChange }> 
                     30 miles 
                 </Button> 
             </ButtonGroup> 
+            </div>
             <br />
             <br />
-            <Link to="/Results"> <Button type="submit"  >Submit</Button> </Link>
+            <Link to="/Results"> <Button type="submit">Submit</Button> </Link>
            
         </form>
             </Well>
